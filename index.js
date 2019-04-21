9a595ab7e198d8cbc8bf6832c73b8ceb6f908152
Emir Eminovic <eeminovi@gmail.com>
2019-04-21 21:24:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase()){
+    var returnStatement="I can't hear you!"
+    return returnStatement;
+  }
+  
+}
